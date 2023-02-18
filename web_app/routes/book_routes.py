3ce11113185a1e1c21f633bf2dab603940546d12
Nour Etienne <nouretienne1@gmail.com>
2023-02-18 14:44:40 +0100

    Render books as JSON

---
+++ web_app/routes/book_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify
+
+book_routes = Blueprint("book_routes", __name__)
+
+@book_routes.route('/books.json')
+def list_books():
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"}
+    ]
+    return jsonify(books)
